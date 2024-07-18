fea93da12306dc5172239b9bb9aaa724b121a288
akheel-zuber <zuber2d25@gmail.com>
2024-07-18 11:25:52 +0530

    Switched to HashRouter

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Alert from './components/Alert';
 import React, { useState } from 'react';
 import About from './components/About';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route
 } from 'react-router-dom';
